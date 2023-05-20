72926214abe9763ea1d243c1051fee579e50be76
TNV-801 <nik701rus@yandex.ru>
2023-05-20 15:51:24 +0300

    Update test_euler_cycle

--- BottleWebProject_C022_ 2_KRT/tests/test_euler_cycle.py
+++ BottleWebProject_C022_ 2_KRT/tests/test_euler_cycle.py
@@ -1,10 +1,13 @@
 import unittest
+import sys
+sys.path.append( '.' )
+from static.model.EulerGraph import EulerGraph
 
 
 class TestPositiveFindCycleEuler(unittest.TestCase):
     """Unittest класс для правильных тестов"""
 
-    def test_1(self):
+    def test_graph_is_euler_1_(self):
         """"""
 
     def test_2(self):
@@ -20,8 +23,17 @@ class TestPositiveFindCycleEuler(unittest.TestCase):
 class TestNegativeFindCycleEuler(unittest.TestCase):
     """Unittest класс для неправильных тестов"""
 
-    def test_1(self):
-        """"""
+    def test_graph_is_euler_2_vertexs_graph_is_not_euler(self):
+        """Unit тест проверки что граф является Эйлеровым. передаёстся 1 вершина. Ожидаемый результат false"""
+        # Arrange
+        matrix = [[0, 0], [0, 0]]
+        expected_answer = False
+
+        # Act
+        actual_answer = EulerGraph.is_eulerian_graph(matrix)
+
+        # Assert
+        self.assertIn(expected_answer, actual_answer)
 
     def test_2(self):
         """"""
