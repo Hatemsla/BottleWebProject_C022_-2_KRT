323bb2fc6dab16c4e268ef28001c37ebf0d84e14
TNV-801 <nik701rus@yandex.ru>
2023-05-20 14:50:37 +0300

    Added a layout for unit tests on the Euler cycle

---
+++ BottleWebProject_C022_ 2_KRT/tests/test_euler_cycle.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+class TestPositiveFindCycleEuler(unittest.TestCase):
+    """Unittest класс для правильных тестов"""
+
+    def test_1(self):
+        """"""
+
+    def test_2(self):
+        """"""
+
+    def test_3(self):
+        """"""
+
+    def test_4(self):
+        """"""
+
+
+class TestNegativeFindCycleEuler(unittest.TestCase):
+    """Unittest класс для неправильных тестов"""
+
+    def test_1(self):
+        """"""
+
+    def test_2(self):
+        """"""
+
+    def test_3(self):
+        """"""
+
+    def test_4(self):
+        """"""
+
+
+if __name__ == '__main__':
+    unittest.main()
